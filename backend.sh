ecb001e4e2baa9d8ab4363ab4a4a36c3f890e995
Preethi <preethi5014@gmail.com>
2024-03-05 15:40:15 +0800

    Update failure state to force exit

---
+++ backend.sh
@@ -0,0 +1,68 @@
+source common.sh
+
+mysql_root_password=$1
+
+# If password is not provided then we will exit
+if [ -z "${mysql_root_password}" ]; then
+  echo Input Password is missing.
+  exit 1
+  fi
+
+Print_Task_Heading "Diasble default NojeJS Version Module"
+dnf module disable nodejs -y &>>LOG
+Check_Status $?
+
+Print_Task_Heading "Enable NodeJS module for v20"
+snf module enable nodejs:20 -y &>>$LOG
+Check_Status $?
+
+Print_Task_Heading "Install NodeJS"
+snf install nodejs -y &>>$LOG
+Check_Status $?
+
+Print_Task_Heading "Adding Application User"
+useradd expense &>>$LOG
+Check_Status $?
+
+Print_Task_Heading "Copy Backend Service file"
+cp backend.servoce /etc/systemd/system/backend.service &>>$LOG
+Check_Status $?
+
+Print_Task_Heading "Clean the Old Content"
+rm -rf /app &>>$LOG
+Check_Status $?
+
+Print_Task_Heading " Create App Directory"
+mkdir /app &>>$LOG
+Check_Status $?
+
+Print_Task_Heading "Download App Directory"
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>>$LOG
+cd /app &>>$LOG
+unzip /tmp/backend.zip &>>$LOG
+Check_Status $?
+
+Print_Task_Heading "Extract App Content"
+cd /app &>>$LOG
+unzip /tmp/backend.zip &>>$LOG
+Check_Status $?
+
+Print_Task_Heading "Download NodeJS Dependencies"
+cd /app &>>$LOG
+npm install &>>$LOG
+Check_Status $?
+
+Print_Task_Heading "Start Backend Service"
+systemctl daemon-reload &>>$LOG
+systemctl enable backend &>>$LOG
+systemctl start backend &>>$LOG
+Check_Status $?
+
+Print_Task_Heading "Install Mysql Client"
+dnf install mysql -y &>>$LOG
+Check_Status $?
+
+Print_Task_Heading "Load Schema"
+mysql -h 172-31-29-157 -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG
+
+Check_Status $?
