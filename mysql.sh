4266cc5cea8e56f0afdd0db21b4e18d96472b4d5
Preethi <preethi5014@gmail.com>
2024-03-05 16:29:32 +0800

    Handling MySQL Password setup failure scenario

--- mysql.sh
+++ mysql.sh
@@ -16,6 +16,9 @@ systemctl start mysqld &>>$LOG
 Check_Status $?
 
 Print_Task_Heading "Setup MySQL Password"
+echo 'show databases' |mysql -h 172.31.29.157 -uroot -p${mysql_root_password} &>>$LOG
+if [ $? -ne 0 ]; tehn
 mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOG
+fi
 Check_Status $?
 
