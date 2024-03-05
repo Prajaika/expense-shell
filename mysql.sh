f0c40d7285b3f3e54bde36523e5f4826d5ece29d
Preethi <preethi5014@gmail.com>
2024-03-05 15:11:33 +0800

    Update failure state to force exit

--- mysql.sh
+++ mysql.sh
@@ -1,4 +1,21 @@
-dnf install mysql-server -y
-systemctl enable mysqld
-systemctl start mysqld
-mysql_secure_installation --set-root-pass ExpenseApp@1
+source common.sh
+
+mysql_root_password=$1
+if [ -z "${mysql_root_password}" ]; then
+  echo Input Password is missing
+exit 1
+fi
+
+Print_Task_Heading "Install Nginx"
+dnf install mysql-server -y &>>$LOG
+Check_Status $?
+
+Print_Task_Heading "Install Nginx"
+systemctl enable mysqld &>>$LOG
+systemctl start mysqld &>>$LOG
+Check_Status $?
+
+Print_Task_Heading "Install Nginx"
+mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOG
+Check_Status $?
+
